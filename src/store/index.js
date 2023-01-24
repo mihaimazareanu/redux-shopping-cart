e6653821522c63a59c73107cf252e73621f9041a
mihaimazareanu <49844133+mihaimazareanu@users.noreply.github.com>
2023-01-24 18:08:40 +0100

    upload project

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import authSlice from "./auth-slice";
+import cartSlice from "./cartSlice";
+
+const store = configureStore({
+    reducer: {
+        auth: authSlice.reducer,
+        cart: cartSlice.reducer,
+    }
+});
+
+export default store;
